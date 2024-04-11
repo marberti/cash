9b4b7db14ffd50da1320faac3725210c50b15a14
Marco Bertini <marco@merlo.localdomain>
2024-04-11 09:35:15 +0200

    added list_ball()

--- main.f90
+++ main.f90
@@ -76,6 +76,8 @@ program main
       else
         write(*,*) "No xyz file loaded"
       end if
+    case ("listball")
+      call list_ball(nuc_in_ball)
     case ("listsort")
       call write_sorted_xyz()
     case ("ls")
@@ -119,6 +121,8 @@ subroutine shell_help()
   write(*,*) "  help, ?                 print this help"
   write(*,*) "  info                    write crystal analysis info"
   write(*,*) "  list                    list xyz nuclei"
+  write(*,*) "  listball                list xyz nuclei in the last generated ball"
+  write(*,*) "                            at increasing distance from center"
   write(*,*) "  listsort                list xyz nuclei at increasing distance from center"
   write(*,*) "  ls [args]               list files"
   write(*,*) "  molden [args]           run molden"
@@ -216,6 +220,29 @@ end subroutine sort_xyz_center
 
 !==============================================================================
 
+subroutine list_ball(nuc_in_ball)
+
+  integer, intent(in) :: nuc_in_ball
+
+  integer :: i
+  integer :: s
+
+  if (xyz_center == 0) then
+    write(*,*) "Please specify a center first"
+    return
+  end if
+
+  write(*,*) "  i     n Nuc      x           y           z           dist"
+  do i = 1, nuc_in_ball
+    s = xyz_sorted_by_dist(i)
+    write(*,'(I4,": ",I4,X,A2,4(2X,F10.4))') &
+      i,s,xyz_e(s),xyz_c(s,1),xyz_c(s,2),xyz_c(s,3),xyz_dist_from_center(s)
+  end do
+
+end subroutine list_ball
+
+!==============================================================================
+
 subroutine write_sorted_xyz()
 
   integer :: i
@@ -226,7 +253,7 @@ subroutine write_sorted_xyz()
     return
   end if
 
-  write(*,*) "  i     Nuc        x           y           z           dist"
+  write(*,*) "  i     n Nuc      x           y           z           dist"
   do i = 1, xyz_a
     s = xyz_sorted_by_dist(i)
     write(*,'(I4,": ",I4,X,A2,4(2X,F10.4))') &
